f90a2b324037b0762682d938f9f5bdb1907231fb
seichan2000 <sei2000h0929@gmail.com>
2022-03-31 12:52:00 +0900

    topic-8 second commit

--- AOJ/08/08_B.py
+++ AOJ/08/08_B.py
@@ -1,9 +1,6 @@
 while True:
-	x = int(input())
-	if x == 0:
+	x = input()
+	if x == "0":
 		break
-	numstr = str(x)
-	sum = 0
-	for i in numstr:
-		sum += int(i)
-	print(sum)
+	s = sum(map(int, x))
+	print(s)
